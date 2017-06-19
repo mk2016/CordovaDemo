6c1042df5fa0c744a2a03738ee173827165243ed
MK <xiaomk7758@sina.com>
2017-06-19 16:41:26 +0800

    bluetooth

---
+++ platforms/ios/CordovaDemo/Plugins/com.mk.testplugin/MKTestPlugin.h
@@ -0,0 +1,13 @@
+//
+//  MKTestPlugin.h
+//  CordovaDemo
+//
+//  Created by xmk on 2017/6/10.
+//
+//
+
+#import <Cordova/CDVPlugin.h>
+
+@interface MKTestPlugin : CDVPlugin
+
+@end
---
+++ platforms/ios/CordovaDemo/Plugins/com.mk.testplugin/MKTestPlugin.m
@@ -0,0 +1,13 @@
+//
+//  MKTestPlugin.m
+//  CordovaDemo
+//
+//  Created by xmk on 2017/6/10.
+//
+//
+
+#import "MKTestPlugin.h"
+
+@implementation MKTestPlugin
+
+@end
