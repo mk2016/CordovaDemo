ccbd12f7f92a4f424f5269592b71aad2ccf68037
MK <xiaomk7758@sina.com>
2017-06-28 10:37:22 +0800

    modify

--- platforms/ios/www/PrinterHelper.js
+++ platforms/ios/www/PrinterHelper.js
@@ -4,7 +4,6 @@ function PrinterHelper(){}
 
 PrinterHelper.prototype.resetInfos = function(){
     printerInfos = [];
-    console.log("count:" + printerInfos.length);
 }
 
 PrinterHelper.prototype.appendText = function (text, alignment, fontType) {
@@ -14,10 +13,6 @@ PrinterHelper.prototype.appendText = function (text, alignment, fontType) {
     infoModel.fontType = fontType;
     infoModel.aligmentType = alignment;
     var count = self.printerInfos.push(infoModel);
-
-    var jsonStr = JSON.stringify(infoModel);
-    console.log("appendText:" + jsonStr);
-    console.log("count:" + count);
 }
 
 PrinterHelper.prototype.appendTextList = function (textList, isTitle, offset) {
@@ -27,10 +22,6 @@ PrinterHelper.prototype.appendTextList = function (textList, isTitle, offset) {
     infoModel.isTitle = isTitle;
     infoModel.offset = offset;
     var count = self.printerInfos.push(infoModel);
-
-    var jsonStr = JSON.stringify(infoModel);
-    console.log("appendTextList:" + jsonStr);
-    console.log("count:" + count);
 }
 
 
@@ -41,10 +32,6 @@ PrinterHelper.prototype.appendBarCode = function (text, alignment, maxWidth){
     infoModel.aligmentType = alignment;
     infoModel.maxWidth = maxWidth;
     var count = self.printerInfos.push(infoModel);
-
-    var jsonStr = JSON.stringify(infoModel)
-    console.log("appendBarCode:" + jsonStr);
-    console.log("count:" + count);
 }
 
 
@@ -55,10 +42,6 @@ PrinterHelper.prototype.appendQrCode = function (text, size, alignment){
     infoModel.aligmentType = alignment;
     infoModel.qrCodeSize = size;
     var count = self.printerInfos.push(infoModel);
-
-    var jsonStr = JSON.stringify(infoModel)
-    console.log("appendBarCode:" + jsonStr);
-    console.log("count:" + count);
 }
 
 PrinterHelper.prototype.appendImage = function (text, maxWidth, alignment){
@@ -68,20 +51,12 @@ PrinterHelper.prototype.appendImage = function (text, maxWidth, alignment){
     infoModel.aligmentType = alignment;
     infoModel.maxWidth = maxWidth;
     var count = self.printerInfos.push(infoModel);
-
-    var jsonStr = JSON.stringify(infoModel)
-    console.log("appendImage:" + jsonStr);
-    console.log("count:" + count);
 }
 
 PrinterHelper.prototype.appendSeperatorLine = function(){
     var infoModel = new Object();
     infoModel.infoType = BTPInfoType.seperatorLine;
     var count = self.printerInfos.push(infoModel);
-
-    var jsonStr = JSON.stringify(infoModel)
-    console.log("appendSeperatorLine:" + jsonStr);
-    console.log("count:" + count);
 }
 
 
@@ -90,35 +65,28 @@ PrinterHelper.prototype.appendFooter = function(text){
     infoModel.infoType = BTPInfoType.footer;
     infoModel.text = text;
     var count = self.printerInfos.push(infoModel);
-
-    var jsonStr = JSON.stringify(infoModel)
-    console.log("appendFooter:" + jsonStr);
-    console.log("count:" + count);
 }
 
 PrinterHelper.prototype.getPrinterInfoJsonString = function(){
     var jsonStr = JSON.stringify(printerInfos);
-    console.log("length:" + printerInfos.length);
-    console.log("jsonString:" + jsonStr);
     return jsonStr;
 }
 
 var printerHelper = new PrinterHelper();
 window.printerInfo = printerHelper;
 
-   // var infoModel = new Object();
-   // infoModel.infoType = BTPInfoType.text;
-   // infoModel.text = text;
-   // infoModel.textArray = ["铅笔刀","2.00","5","10.00"];
-   // infoModel.fontType = MKBTPFontType.middle;
-   // infoModel.aligmentType = MKBTPAlignmentType.center;
-   // infoModel.maxWidth = 300;
-   // infoModel.qrCodeSize = 12;
-   // infoModel.offset = 150;
-   // infoModel.isTitle = 0;
-   // var jsonStr = JSON.stringify(infoModel)
-   // console.log(jsonStr)
-   // console.log("appendText");
+// 所有参数
+// var infoModel = new Object();
+// infoModel.infoType = BTPInfoType.text;
+// infoModel.text = text;
+// infoModel.textArray = ["铅笔刀","2.00","5","10.00"];
+// infoModel.fontType = MKBTPFontType.middle;
+// infoModel.aligmentType = MKBTPAlignmentType.center;
+// infoModel.maxWidth = 300;
+// infoModel.qrCodeSize = 12;
+// infoModel.offset = 150;
+// infoModel.isTitle = 0;
+
 
 
 
